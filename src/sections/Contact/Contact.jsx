bff80bdc301fc4da656f5f43321f108aff315f03
Yiming Zhu <yiming168@gmail.com>
2024-08-21 03:35:01 -0700

    add form action and post method

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formsubmit.co/yiming168@gmail.com" method="POST">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
